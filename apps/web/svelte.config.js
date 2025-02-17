f78bd0a4c733e4edbab182a93148305a6ce1431f
imjlk <i@m.junglei.kim>
2025-02-17 18:39:32 +0900

    Configure Cloudflare Pages asset caching and simplify SvelteKit adapter

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -8,21 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			// routes: {
-			// 	include: ['/*'],
-			// 	exclude: ['<all>']
-			// },
-			// platformProxy: {
-			// 	configPath: 'wrangler.toml',
-			// 	environment: undefined,
-			// 	experimentalJsonConfig: false,
-			// 	persist: false
-			// }
-		}),
+		adapter: adapter(),
 		files: {
 			routes: "src/app",
 			lib: "src",
